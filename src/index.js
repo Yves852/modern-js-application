9c53dbb1c6dd0703107738230f79062a71df05f9
Yves Thirion <thirion.yves@gmail.com>
2021-08-19 16:26:38 +0200

    Updated index.js
    Add edit.js module, handle url pathing for index.html and edit.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { displayCharacters } from "./displayCharacters.js";
+import { prepareEdit } from "./edit.js";
 
 // Semaphore to control access to checkBarGet()
 let researchRequestSemaphore = true;
@@ -12,22 +13,27 @@ const checkBarGet = ()=>{
 }
 
 (() => {
-  // On load, fill the pool section with cards from characters
-  displayCharacters();
-  // Empty bar at load
-  searchBar.value = "";
-  // When release key on the search bar, make a research if none already launch within 500 ms
-  // researchRequestSemaphore control the access to the function, so it cannot be triggered more than 
-  // one time every 500 ms and prevent API to die of requests
-  searchBar.addEventListener("keyup", async ()=>{
-    if(researchRequestSemaphore) {
-      researchRequestSemaphore = false;      
-      setTimeout(checkBarGet, 500);
-      researchRequestSemaphore = true;
-    }
-  });
-  // Add button onclick, open edit page to post a new character
-  document.getElementById('addBtn').addEventListener("click", () => {
-    window.open('./html/edit.html', "_self")
-  });
+  if(window.location.pathname == "/dist/"  || window.location.pathname == "/dist/index.html"){
+    // On load, fill the pool section with cards from characters
+    displayCharacters();
+    // When release key on the search bar, make a research if none already launch within 500 ms
+    // researchRequestSemaphore control the access to the function, so it cannot be triggered more than 
+    // one time every 500 ms and prevent API to die of requests
+    searchBar.value = ""; 
+    searchBar.addEventListener("keyup", async ()=>{
+      if(researchRequestSemaphore) {
+        researchRequestSemaphore = false;      
+        setTimeout(checkBarGet, 500);
+        researchRequestSemaphore = true;
+      }  
+    });    
+    // Add button onclick, open edit page to post a new character
+    document.getElementById('addBtn').addEventListener("click", () => {
+      window.open('./edit.html', "_self")
+    });
+  }
+  else if(window.location.pathname == "/dist/edit.html") {
+    prepareEdit();
+  }
+  else console.error("unknow path", window.location.pathname);
 })();
